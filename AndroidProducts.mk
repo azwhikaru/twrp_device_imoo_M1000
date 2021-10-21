a3150f68b35192731f99bd08dff5fb275195f56c
Aizawa Hikaru <37921907+azwhikaru@users.noreply.github.com>
2021-10-21 11:33:47 +0800

    Initial Android 6.0
    Initial Android 6.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_M1000.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_M1000-user \
+    omni_M1000-userdebug \
+    omni_M1000-eng
